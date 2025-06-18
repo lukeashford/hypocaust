efbecf786922c93f9f0f5560f2e4d0350586ba61
lwa <l.waclawczyk@imp-ag.de>
2025-06-18 14:35:11 +0200

    Initializr

---
+++ src/main/java/com/example/the_machine/TheMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMachineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TheMachineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/the_machine/TheMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMachineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
